bf996b5ce4cb11a24be0e80df9b603fc13b7ec94
Vatsal Parekh <contact@vatsalparekh.com>
2020-03-11 16:10:46 -0400

    setup py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="algorithm",
+    version="0.0.1",
+    description="Some common algorithms implemented",
+    url="http://github.com/vatsalp/algorithm",
+    author="Vatsal Parekh",
+    author_email="contact@vatsalparekh.com",
+    license="MIT",
+    packages=["algorithm"],
+    zip_safe=False,
+)
